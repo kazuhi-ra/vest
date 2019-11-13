8edd705e608ed2e049abd104c7006c2dc492bc74
kazuhi_ra <kazuhira4126@gmail.com>
2019-11-13 18:43:13 +0900

    canvasの実装が成功

---
+++ public/js/canvas-vest.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let c = document.getElementById("myCanvas");
+let ctx = c.getContext("2d");
+let imageObj1 = new Image();
+let imageObj2 = new Image();
+let imageObj3 = new Image();
+imageObj1.src = "../images/vest_head.png";
+
+imageObj1.onload = function () {
+  ctx.drawImage(imageObj1, 0, 0, 100, 50);
+
+  imageObj2.src = "../images/vest_01.png";
+  imageObj2.onload = function () {
+    ctx.drawImage(imageObj2, 0, 50, 100, 50);
+
+    imageObj3.src = "../images/vest_foot.png";
+    imageObj3.onload = function () {
+      ctx.drawImage(imageObj3, 0, 100, 100, 50);
+      // let sanmi = c.toDataURL("image/png");
+      // document.write(`<img src=sanmi>`);
+      let dataURL = c.toDataURL();
+      // let output = document.getElementById("output");
+      // output.src = dataURL;
+    };
+  };
+};
